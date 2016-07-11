672620f8f4f8848cf07951e37be8c3ce36fd183e
Ian Golden <ian@iangolden.com>
2016-07-10 22:49:36 -0600

    initial

---
+++ acf-bundle.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Acf-bundle
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: acf-bundle
+ * Domain Path: /languages
+ * @package Acf-bundle
+ */
